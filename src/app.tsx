46cc0ee3e1c92997aa4afcfd80b3c83a5dea0017
princejoogie <princejoogie@gmail.com>
2023-05-15 18:29:20 +0800

    feat: stream responses

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import https from "https";
 import { Spinner, TextInput } from "@inkjs/ui";
 import { Text, Box } from "ink";
-import { openai } from "./config.js";
 import { Result } from "meow";
 
 type Message = {
@@ -10,26 +10,6 @@ type Message = {
 };
 
 let inputIdx = 0;
-
-const getResponse = async (content: string, messages: Message[]) => {
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [...messages, { role: "user", content }],
-    temperature: 0,
-  });
-
-  if (response.data.choices.length <= 0) {
-    console.error("Error: No choices returned");
-    process.exit(1);
-  }
-
-  if (!response.data.choices[0]?.message?.content) {
-    console.error("Error: No output returned");
-    process.exit(1);
-  }
-
-  return response.data.choices[0].message.content;
-};
 
 const MessageItem = ({ msg }: { msg: Message }) => {
   return (
@@ -50,7 +30,8 @@ const MessageItem = ({ msg }: { msg: Message }) => {
 };
 
 export default function App({ cli }: { cli: Result<{}> }) {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const [chunkedResponse, setChunkedResponse] = useState("");
   const [messages, setMessages] = useState<Array<Message>>([
     {
       role: "system",
@@ -58,13 +39,75 @@ export default function App({ cli }: { cli: Result<{}> }) {
     },
   ]);
 
-  const onSubmit = async (content: string) => {
-    setIsLoading(true);
-    setMessages((old) => [...old, { role: "user", content }]);
-    const res = await getResponse(content, messages);
-    setIsLoading(false);
-    setMessages((old) => [...old, { role: "assistant", content: res }]);
+  const getStreamedResponse = (content: string, messages: Message[]) => {
+    if (!process.env["OPENAI_API_KEY"]) {
+      console.log("OPENAI_API_KEY key missing");
+      process.exit(1);
+    }
+
+    const req = https.request(
+      {
+        hostname: "api.openai.com",
+        port: 443,
+        path: "/v1/chat/completions",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env["OPENAI_API_KEY"],
+        },
+      },
+      function (res) {
+        res.on("data", (chunk) => {
+          if (isDone) {
+            setIsDone(false);
+          }
+
+          const raw: string = chunk.toString();
+          if (raw.includes("[DONE]")) return;
+
+          const rawArray = raw
+            .replace(/data: {/gi, "{")
+            .split("\n")
+            .filter((e) => e !== "");
+
+          rawArray.forEach((e) => {
+            const parsed = JSON.parse(e);
+            if (parsed.choices[0].delta.content) {
+              setChunkedResponse((e) => e + parsed.choices[0].delta.content);
+            }
+          });
+        });
+        res.on("end", () => {
+          setIsDone(true);
+        });
+      }
+    );
+
+    const body = JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [...messages, { role: "user", content }],
+      temperature: 0.0,
+      stream: true,
+    });
+
+    req.on("error", (e) => {
+      console.error("problem with request:" + e.message);
+    });
+
+    req.write(body);
+
+    req.end();
   };
+
+  useEffect(() => {
+    if (isDone && !!chunkedResponse) {
+      setMessages((old) => [
+        ...old,
+        { role: "assistant", content: chunkedResponse },
+      ]);
+      setChunkedResponse("");
+    }
+  }, [isDone, chunkedResponse]);
 
   return (
     <Box flexDirection="column" padding={2}>
@@ -73,17 +116,21 @@ export default function App({ cli }: { cli: Result<{}> }) {
           <MessageItem key={`message-${idx}`} msg={msg} />
         ))}
 
-        {isLoading && (
+        {!!chunkedResponse && (
           <Box
-            borderColor="blue"
+            borderColor="red"
             borderStyle="single"
             borderTop={false}
             borderBottom={false}
             borderLeft={true}
             borderRight={false}
           >
-            <Text color="blue">{"Gpt:  "}</Text>
-            <Spinner label="Loading..." />
+            <Box>
+              <Text>Gpt:</Text>
+              <Spinner />
+              <Text> </Text>
+            </Box>
+            <Text>{chunkedResponse}</Text>
           </Box>
         )}
       </Box>
@@ -97,15 +144,16 @@ export default function App({ cli }: { cli: Result<{}> }) {
           placeholder="Send a message."
           onSubmit={async (content) => {
             inputIdx++;
-            onSubmit(content);
+            setMessages((old) => [...old, { role: "user", content }]);
+            getStreamedResponse(content, messages);
           }}
         />
       </Box>
 
       <Box paddingX={1}>
         <Text color="grey">
-          chatgpt-cli v{cli.pkg.version} • by Prince Carlo Juguilon •{" "}
-          © {new Date().getFullYear()}
+          chatgpt-cli v{cli.pkg.version} • by Prince Carlo Juguilon • ©{" "}
+          {new Date().getFullYear()}
         </Text>
       </Box>
     </Box>
